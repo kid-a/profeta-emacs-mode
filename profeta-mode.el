f8a6403fe46eca6c762bd0f826f438ee4c40c879
Loris Fichera <loris.fichera@gmail.com>
2011-08-06 18:12:53 +0200

    highlighting done

--- profeta-mode.el
+++ profeta-mode.el
@@ -19,42 +19,21 @@
 (setq my-keywords
       `(
 	;; addition and deletion of goals and beliefs:
-	( "-~?\[a-zA-Z\]+()" .  font-lock-keyword-face )
-	( "+~?\[a-zA-Z\]+()" .  font-lock-keyword-face )
-
-	;; override some python operators
-	( ,(regexp-opt '(">>") 'word) . font-lock-keyword-face)
-
-
-	;; (,mylsl-type-regexp . font-lock-type-face)
-	;; (,mylsl-constant-regexp . font-lock-constant-face)
-	;; (,mylsl-event-regexp . font-lock-builtin-face)
-	;; (,mylsl-functions-regexp . font-lock-function-name-face)
-	;; (,mylsl-keywords-regexp . font-lock-keyword-face)
+	( "-[~]?[a-zA-Z_][a-zA-Z0-9_]*" . font-lock-warning-face )
+	( "+[~]?[a-zA-Z_][a-zA-Z0-9_]*" . font-lock-type-face )
+	
+	;; some reserved words
+	( ,(regexp-opt '("def " "from " "import " "declare_episode")) . font-lock-preprocessor-face )
+	
+	;; the '>>' symbol
+	( ,(regexp-opt '(">>") 'word) . font-lock-builtin-face)
 	
 	;; numbers
-	( "-? [0-9]+ (\.[0-9]+)? ([eE][+-]?[0-9]+)?" . font-lock-keyword-face )
-	
-
-	;; python operators
-  	;; ( ,(regexp-opt '("[" "]" "|" ">>") 'word) . font-lock-warning-face)
-	;; ( ,(regexp-opt '(">" "<" ">=" "<=" "==") 'word) . font-lock-keyword-face)
-
-	;; actions into plans:
-	;; ( " \[a-zA-Z0-9\]*(.*),\\| \[a-zA-Z0-9\]*(.*)$\\| \[a-zA-Z0-9\]*(.*)\t" . font-lock-variable-name-face)
-	;; ( ,(regexp-opt '("and" "not" "or" "is" "lambda") 'words) . font-lock-keyword-face)
-	;; ( ,(regexp-opt '("!" "&") 'word) . font-lock-keyword-face)
-	;; ;; variables
-	;; ( ,(regexp-opt '("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z") 'words) . font-lock-variable-name-face)
-	;; ;; more variables
-	;; ( "\\<\[a-zA-Z0-9\]\[a-z0-9\]\\>" . font-lock-variable-name-face)
-	;; ;; atoms
-	;; ( "\\<\[a-z\]*\\>" . font-lock-variable-name-face)
-	;; ;; addition and deletion of beliefs:
-	;; ( "\+" .  font-lock-constant-face )
-	;; ( "\-" .  font-lock-constant-face )
+	( "\\b[0-9]*[\.]?[0-9]+" . font-lock-constant-face )
 	)
       )
+
+;; add command shortcut for comments
 
 ;; define profeta-mode
 (define-derived-mode profeta-mode python-mode "Profeta Strategy"
@@ -63,7 +42,7 @@
   ;; ;; modify the keymap
   ;; (define-key profeta-mode-map [remap comment-dwim] 'profeta-comment-dwim)
   
-  ;; ;; python style comment: “# ...” 
+  ;; ;; python style comment: “# ...”
   ;; (modify-syntax-entry ?# "< b" profeta-mode-syntax-table)
   ;; (modify-syntax-entry ?\n "> b" profeta-mode-syntax-table)
   )
